3387b08a69c8e34a2f8864db7a08f7ac6dbb26a0
samuel <amit.samuel@web.de>
2020-05-26 10:55:19 +0200

    remove all js code

--- js/scripts.js
+++
@@ -1,7 +0,0 @@
-alert("Hallo World");
-// adding my first var
-var favoriteFood = "watermelon";
-document.write(favoriteFood);
-
-var secendFavoritFood = "Pizza";
-document.write(secendFavoritFood);
