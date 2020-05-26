3ffa512e5301d3299b6c6a36c0e1b195e2ce2211
samuel <amit.samuel@web.de>
2020-05-26 20:30:42 +0200

    add loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,28 @@
 var pokemonList = [
-    {name: 'Bulbasaur', heigth: 7, type: ['grass', 'poison']},
-    {name: 'Venusaur', heigth: 2, type: ['Monster', 'Grass']},
-    {name: 'Charizard', heigth: 1.7 , type: ['Monster', 'Dragon']},
-    {name: 'Blastoise', heigth: 1.6, type: ['Monster', 'Water' ]}];
+    {
+        name: 'Bulbasaur',
+        heigth: 7,
+        type: ['grass', 'poison']
+    },
+    {
+        name: 'Venusaur',
+        heigth: 2,
+        type: ['Monster', 'Grass']
+    },
+    {
+        name: 'Charizard',
+        heigth: 1.7 ,
+        type: ['Monster', 'Dragon']
+    },
+    {
+        name: 'Blastoise',
+        heigth: 1.6,
+        type: ['Monster', 'Water' ]
+    }
+    ];
+
+
+for(i = 0; i < pokemonList.length; i++){
+    document.write(pokemonList[i].name);
+    console.log(pokemonList[i].name);
+}
