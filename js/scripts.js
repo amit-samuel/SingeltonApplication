6b4f7ff0e7d064317d8d1ac726faa0dc6dcb4e8f
samuel <amit.samuel@web.de>
2020-05-26 21:33:41 +0200

    add function for tableRow add string for height

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,39 @@
-alert("Hallo World");
-// adding my first var
-var favoriteFood = "watermelon";
-document.write(favoriteFood);
+var pokemonList = [
+    {
+        name: 'Bulbasaur',
+        heigth: 0.6,
+        type: ['grass', 'poison']
+    },
+    {
+        name: 'Venusaur',
+        heigth: 0.8,
+        type: ['Monster', 'Grass']
+    },
+    {
+        name: 'Charizard',
+        heigth: 1.7,
+        type: ['Monster', 'Dragon']
+    },
+    {
+        name: 'Blastoise',
+        heigth: 1.6,
+        type: ['Monster', 'Water' ]
+    }
+    ];
 
-var secendFavoritFood = "Pizza";
-document.write(secendFavoritFood);
+function myFunction(a) {
+    var table = document.getElementById("myTable");
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    cell1.innerHTML = a;
+}
+
+
+for(i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].heigth > 1) {
+        document.write(myFunction(pokemonList[i].name + "(" + "height: " + pokemonList[i].heigth + ")" + "- Wow, that’s big!"));
+    } else {
+        document.write(myFunction(pokemonList[i].name + "(" + "height: " + pokemonList[i].heigth + ")"));
+    }
+}
+
