7624ad4688461fdba2e7e59c3227b4dfd51f56ab
samuel <amit.samuel@web.de>
2020-05-26 18:29:09 +0200

    add some pokemon key and values

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var pokemonList = [
+    {name: 'Bulbasaur', heigth: 7, type: ['grass', 'poison']},
+    {name: 'Venusaur', heigth: 2, type: ['Monster', 'Grass']},
+    {name: 'Charizard', heigth: 1.7 , type: ['Monster', 'Dragon']},
+    {name: 'Blastoise', heigth: 1.6, type: ['Monster', 'Water' ]}];
