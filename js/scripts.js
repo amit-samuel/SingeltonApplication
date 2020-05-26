07a8aa4eeaf647e1909aef919a5a11342a008623
samuel <amit.samuel@web.de>
2020-05-26 09:12:27 +0200

    Add alert add first var

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+alert("Hallo World");
+// adding my first var
+var favoriteFood = "watermelon";
+document.write(favoriteFood);
+
+var secendFavoritFood = "Pizza";
+document.write(secendFavoritFood);
