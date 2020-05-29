44658df6f738291183767e838532cd6215f1b7ec
samuel <amit.samuel@web.de>
2020-05-30 01:19:04 +0200

    add forEach add repository add func add getAll

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,49 @@
-var pokemonList = [
-    {
-        name: 'Bulbasaur',
-        heigth: 0.6,
-        type: ['grass', 'poison']
-    },
-    {
-        name: 'Venusaur',
-        heigth: 0.8,
-        type: ['Monster', 'Grass']
-    },
-    {
-        name: 'Charizard',
-        heigth: 1.7,
-        type: ['Monster', 'Dragon']
-    },
-    {
-        name: 'Blastoise',
-        heigth: 1.6,
-        type: ['Monster', 'Water' ]
-    }
+var pokemonRepository = (function () {
+    var pokemonList = [
+        {
+            name: 'Bulbasaur',
+            heigth: 0.6,
+            type: ['grass', 'poison']
+        },
+        {
+            name: 'Venusaur',
+            heigth: 0.8,
+            type: ['Monster', 'Grass']
+        },
+        {
+            name: 'Charizard',
+            heigth: 1.7,
+            type: ['Monster', 'Dragon']
+        },
+        {
+            name: 'Blastoise',
+            heigth: 1.6,
+            type: ['Monster', 'Water' ]
+        }
     ];
 
+    function add(item) {
+        pokemonList.push(item);
+    }
 
-Object.keys(pokemonList).forEach(function (property) {
-console.log(pokemonList[property]);
-});
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+
+})();
+
+function myLoopFunction(pok){
+    return (pok.heigth > 1 ?
+        console.log( pok.name + "(" + "height: " + pok.heigth + ")" + "- Wow, that’s big!"):
+    console.log(pok.name + "(" + "height: " + pok.heigth + ")")
+);
+}
+//console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: 'Pikachu' });
+var a = pokemonRepository.getAll();
+console.log(a.forEach(myLoopFunction));
